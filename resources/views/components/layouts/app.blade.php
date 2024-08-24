ab940fe77b99b54fe9121cb4a0ab970e6f4ff8cd
raphael0627f <raphaelferreirafo@gmail.com>
2024-08-24 16:40:16 -0300

    implementado nova home

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,26 +1,3 @@
 <!DOCTYPE html>
-{{-- <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"> --}}
- <html lang="pt-br">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>{{ $title ?? 'Charme Jóias' }}</title>
-    @livewireStyles
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-</head>
-
-<body class="bg-slate-200 dark:bg-slate-700">
-    @livewire('partials.navbar')
-    <main>
-        {{ $slot }}
-    </main>
-    @livewire('partials.footer')
-    @livewireScripts
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <x-livewire-alert::scripts />
-</body>
-
+    {{ $slot }}
 </html>
